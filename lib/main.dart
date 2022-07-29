8ed70fd6207526a6c99af482aafc3a8e79c9c15c
DarkReunion <vue@tom.com>
2022-07-29 10:43:20 +0800

    标题

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '我好兴奋',
+      title: '我好兴奋啊哈哈',
       theme: ThemeData(
         // This is the theme of your application.
         //
