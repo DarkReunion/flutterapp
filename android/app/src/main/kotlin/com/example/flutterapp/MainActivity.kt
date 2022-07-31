fe5ee18227c98971a10bc55dfcd47c6413d47835
DarkReunion <vue@tom.com>
2022-08-01 04:10:17 +0800

    悬浮窗

--- android/app/src/main/kotlin/com/example/flutterapp/MainActivity.kt
+++
@@ -1,2 +0,0 @@
-package com.example.flutterapp
-class MainActivity : com.example.flutterapp.AndroidWindowActivity()
--- android/app/src/main/kotlin/com/example/flutterapp/MainApplication.kt
+++
@@ -1,3 +0,0 @@
-package com.example.flutterapp // same as MainActivity.kt
-
-class MainApplication : com.example.flutterapp.AndroidWindowApplication()
